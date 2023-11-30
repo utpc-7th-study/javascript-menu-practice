3a0f3f537b1f659d64460149d410db280a024c07
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-11-30 21:50:00 +0900

    fix: pickNumberInRange 파라미터 타입 숫자로 변경

--- src/domains/RandomNumber.js
+++ src/domains/RandomNumber.js
@@ -1,9 +1,8 @@
 import { Random } from '@woowacourse/mission-utils';
-import { CATEGORIES } from '../constants/category.js';
 
 const RandomNumber = {
   create() {
-    return Random.pickNumberInRange(1, CATEGORIES.LENGTH);
+    return Random.pickNumberInRange(1, 5);
   },
 };
 
