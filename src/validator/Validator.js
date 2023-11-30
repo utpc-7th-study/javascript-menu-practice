5252c17c6fa94bbd8de9c5940f6adf53e8962c61
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-11-30 22:56:27 +0900

    feat: 메뉴판에 존재하지 않는 메뉴 입력값에 대한 예외처리 구현

--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -1,3 +1,18 @@
+import { MENUS } from '../constants/menu.js';
+
+const isIncludedInMenus = (menu) => {
+  let isIncluded = false;
+  Object.entries(MENUS).forEach(([_, menus]) => {
+    const menuArray = menus.split(', ');
+    if (menuArray.includes(menu)) {
+      isIncluded = true;
+      return false;
+    }
+  });
+
+  return isIncluded;
+};
+
 const Validator = {
   nameLengthOf(coaches) {
     if (coaches.some((name) => name.length < 2 || name.length > 4)) {
@@ -17,6 +32,14 @@ const Validator = {
     }
   },
 
+  notIncluded(menus) {
+    menus.forEach((menu) => {
+      if (!isIncludedInMenus(menu)) {
+        throw new Error('[ERROR] 메뉴판에 존재하지 않는 메뉴입니다.');
+      }
+    });
+  },
+
   duplicationOf(inputs) {
     if (new Set(inputs).size !== inputs.length) {
       throw new Error('[ERROR] 동일한 값을 중복하여 입력했습니다.');
@@ -32,5 +55,6 @@ export const validateCoaches = (coaches) => {
 
 export const validateMenus = (menus) => {
   Validator.numberOfMenus(menus);
+  Validator.notIncluded(menus);
   Validator.duplicationOf(menus);
 };
