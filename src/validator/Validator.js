6042dec16eaa85acf2dc332db3e5c19d7ec8a37f
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-11-30 22:35:26 +0900

    feat: 동일한 입력값에 대한 예외처리 구현

--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -1,13 +1,19 @@
 const Validator = {
   nameLengthOf(coaches) {
     if (coaches.some((name) => name.length < 2 || name.length > 4)) {
-      throw new Error('코치의 이름은 2자 이상, 4자 이하여야 합니다.');
+      throw new Error('[ERROR] 코치의 이름은 2자 이상, 4자 이하여야 합니다.');
     }
   },
 
   numbersOf(coaches) {
     if (coaches.length < 2 || coaches.length > 5) {
-      throw new Error('코치의 수는 2명 이상, 5명 이하여야 합니다.');
+      throw new Error('[ERROR] 코치의 수는 2명 이상, 5명 이하여야 합니다.');
+    }
+  },
+
+  duplicationOf(inputs) {
+    if (new Set(inputs).size !== inputs.length) {
+      throw new Error('[ERROR] 동일한 값을 중복하여 입력했습니다.');
     }
   },
 };
@@ -15,6 +21,9 @@ const Validator = {
 export const validateCoaches = (coaches) => {
   Validator.nameLengthOf(coaches);
   Validator.numbersOf(coaches);
+  Validator.duplicationOf(coaches);
 };
 
-export const validateMenus = (menus) => {};
+export const validateMenus = (menus) => {
+  Validator.duplicationOf(menus);
+};
