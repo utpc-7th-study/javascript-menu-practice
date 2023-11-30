8963247437e0b70910c5e67fde4bd34fe471a42d
rlaclghks123 <rlaclghks123@naver.com>
2023-11-30 18:36:49 +0900

    feat: 유효성검사 - 코치이름이 빈값인 경우 유효성 검사

--- src/domain/Coach.js
+++ src/domain/Coach.js
@@ -2,7 +2,18 @@ class Coach {
   #coachName;
 
   constructor(coachName) {
+    this.#validate(coachName);
     this.#coachName = coachName;
+  }
+
+  #validate(coachName) {
+    this.#validateEmpty(coachName);
+  }
+
+  #validateEmpty(coachName) {
+    if (coachName.trim('') === '') {
+      throw new Error('[ERROR] 빈값은 입력할 수 없습니다.');
+    }
   }
 }
 
