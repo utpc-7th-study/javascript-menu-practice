21a98946302918103b94cbc76c0b82eadeaef060
rlaclghks123 <rlaclghks123@naver.com>
2023-11-30 18:40:09 +0900

    feat: 유효성검사 - 코치이름이 글자수 제한 유효성 검사
    2글자~4글자 사이만 가능

--- src/domain/Coach.js
+++ src/domain/Coach.js
@@ -8,11 +8,22 @@ class Coach {
 
   #validate(coachName) {
     this.#validateEmpty(coachName);
+    this.#validateCoachNameLength(coachName);
   }
 
   #validateEmpty(coachName) {
     if (coachName.trim('') === '') {
       throw new Error('[ERROR] 빈값은 입력할 수 없습니다.');
+    }
+  }
+
+  #validateCoachNameLength(coachName) {
+    if (coachName.length < 2) {
+      throw new Error('[ERROR] 코치의 이름은 2글자 ~ 4글자만 가능합니다.');
+    }
+
+    if (coachName.length > 4) {
+      throw new Error('[ERROR] 코치의 이름은 2글자 ~ 4글자만 가능합니다.');
     }
   }
 }
