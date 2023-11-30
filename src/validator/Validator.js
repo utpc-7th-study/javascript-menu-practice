3823f327da0a118bf4f44ebe3678470364384ff4
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-11-30 22:31:24 +0900

    feat: 코치의 수에 대한 예외처리 구현

--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -1,13 +1,20 @@
 const Validator = {
-  nameLength(coaches) {
+  nameLengthOf(coaches) {
     if (coaches.some((name) => name.length < 2 || name.length > 4)) {
-      throw new Error('코치의이름은 2자 이상, 4자 이하여야 합니다.');
+      throw new Error('코치의 이름은 2자 이상, 4자 이하여야 합니다.');
+    }
+  },
+
+  numbersOf(coaches) {
+    if (coaches.length < 2 || coaches.length > 5) {
+      throw new Error('코치의 수는 2명 이상, 5명 이하여야 합니다.');
     }
   },
 };
 
 export const validateCoaches = (coaches) => {
-  Validator.nameLength(coaches);
+  Validator.nameLengthOf(coaches);
+  Validator.numbersOf(coaches);
 };
 
 export const validateMenus = (menus) => {};
